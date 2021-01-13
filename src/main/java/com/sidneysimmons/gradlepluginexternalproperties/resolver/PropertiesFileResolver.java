7ea0cb8785c18b0ffb271f6264741b31241d4f75
Sidney Simmons <sas.simm@gmail.com>
2021-01-12 21:24:27 -0700

    #35 Remove the "getProperties()" method from the PropertyResolver interface

--- src/main/java/com/sidneysimmons/gradlepluginexternalproperties/resolver/PropertiesFileResolver.java
+++ src/main/java/com/sidneysimmons/gradlepluginexternalproperties/resolver/PropertiesFileResolver.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.MessageFormat;
 import java.util.Properties;
-import org.gradle.api.Project;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,38 +16,21 @@ public class PropertiesFileResolver implements PropertyResolver {
 
     private static final Logger log = LoggerFactory.getLogger(PropertiesFileResolver.class);
 
-    private Project project;
     private File file;
     private Properties properties;
 
     /**
      * Constructor.
      * 
-     * @param project the project
      * @param file the properties file
      */
-    public PropertiesFileResolver(Project project, File file) {
-        this.project = project;
+    public PropertiesFileResolver(File file) {
         this.file = file;
     }
 
     @Override
     public String resolve(String propertyName) {
         return getProperties().getProperty(propertyName);
-    }
-
-    @Override
-    public Properties getProperties() {
-        if (properties == null) {
-            properties = new Properties();
-            try (InputStream inputStream = new FileInputStream(file)) {
-                properties.load(inputStream);
-            } catch (IOException e) {
-                // This is a valid scenario so log it and continue gracefully
-                log.debug(MessageFormat.format("Cannot load external properties file. Continuing gracefully. file = {0}", file));
-            }
-        }
-        return properties;
     }
 
     @Override
@@ -59,6 +41,25 @@ public class PropertiesFileResolver implements PropertyResolver {
         builder.append("file=" + file);
         builder.append(")");
         return builder.toString();
+    }
+
+    /**
+     * Get the properties for this properties file resolver. Loads the properties from the file and
+     * keeps them in memory.
+     * 
+     * @return the properties from the given file
+     */
+    private Properties getProperties() {
+        if (properties == null) {
+            properties = new Properties();
+            try (InputStream inputStream = new FileInputStream(file)) {
+                properties.load(inputStream);
+            } catch (IOException e) {
+                // This is a valid scenario so log it and continue gracefully
+                log.debug(MessageFormat.format("Cannot load external properties file. Continuing gracefully. file = {0}", file));
+            }
+        }
+        return properties;
     }
 
 }
